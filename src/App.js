4f1615eab0bf00a704429a5f4d98b79b7b324f3b
Retrojb <baltescartography@gmail.com>
2021-11-19 13:02:24 -0500

    feature/added app nav bar basic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import BeerTable from './components/BeerTable.js';
+import NavBar from './components/NavBar';
 
 function App() {
 
   return (
     <div className="App">
+      <NavBar />
       <BeerTable />
     </div>
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import Badge from '@mui/material/Badge';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+
+const NavBar = () => {
+    return (
+        <Box sx={{ flexGrow: 1}} >
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="open menu"
+                        sx={{ mr: 2}}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    )
+}
+
+export default NavBar
